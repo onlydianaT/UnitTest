e22f7f17b687c8c9bb7f3729deeea09215dc66e7
Diana Bayazitova <onlydi@mail.ru>
2022-08-08 00:57:49 +0500

    Added folder src/

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -3,31 +3,28 @@ import java.util.Random;
 
 public class MagicBox<T> {
 
-
     protected int amountOfObject;
+    protected T[] items;
+    private Random random = new Random();
 
     public MagicBox(int amountOfObject) {
         this.amountOfObject = amountOfObject;
-        T[] items = (T[]) new Object[amountOfObject];
+        items = (T[]) new Object[amountOfObject];
     }
-
-    T[] items = (T[]) new Object[4];
 
     public boolean add(T item) {
         for (int i = 0; i < items.length; i++) {
             if (items[i] == null) {
                 items[i] = item;
-
                 return true;
             }
-
         }
         System.out.println("Mest dlya dobavleniya new object net");
-        System.out.println(Arrays.deepToString(items));
+        System.out.println(Arrays.toString(items));
         return false;
     }
 
-    public <T> T pick() {
+    public T pick() {
         int count = 0;
         for (int i = 0; i < items.length; i++) {
             if (items[i] == null) {
@@ -36,17 +33,11 @@ public class MagicBox<T> {
         }
         if (count != 0) {
             throw new RuntimeException("Box is not full, free cells are " + count + " shtuk");
-        }
-
-        if (count == 0) {
-            Random random = new Random();
+        } else {
             int randomInt = random.nextInt(amountOfObject);
-            T choosenItem = (T) items[randomInt];
+            T choosenItem = items[randomInt];
             System.out.println("Vibor sohranennogo elementa metodom Random: " + choosenItem);
             return (choosenItem);
-
         }
-
-        return null;
     }
 }
